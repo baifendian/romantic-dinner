16ee59bff514924c6775bc888c942a12aff2ae03
jiangtl <274351128@qq.com>
2016-07-10 00:25:18 +0800

    update app

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// Zhaoshangzhengquan App
+// romantic-dinner App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'dinner' is the name of this angular module example (also set in a <body> attribute in index.html)
